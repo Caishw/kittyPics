3770f1242fcbb0df2d7cbabccc626474748e724d
caishw <aishunairchandran@gmail.com>
2017-05-06 19:14:31 +1000

    Gradle setup

---
+++ src/main/java/com/github/kittypics/config/App.java
@@ -0,0 +1,14 @@
+package com.github.kittypics.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by aishu on 6/05/2017.
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
